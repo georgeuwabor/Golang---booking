fd8104dd06a26925952695519c582ab445ccd264
georgeuwabor <63528996+georgeuwabor@users.noreply.github.com>
2023-08-20 20:20:01 -0400

    pointer

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets int = 50
+	
+	
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+	
+	
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+	
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+	
+	fmt.Println("Enter number of Tickets: ")
+	fmt.Scan(&userTickets)
+	
+	remainingTickets = remainingTickets - userTickets
+	
+	
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+}
