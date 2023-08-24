c012ebfa01f3d690200c34ddd1ecc938a0ecf242
georgeuwabor <63528996+georgeuwabor@users.noreply.github.com>
2023-08-24 17:46:58 -0400

    input-validation

--- main.go
+++ main.go
@@ -33,7 +33,11 @@ func main() {
 		fmt.Println("Enter number of Tickets: ")
 		fmt.Scan(&userTickets)
 		
-		if userTickets < remainingTickets {
+		isValidName := len(firstName) >= 2 && len(lastName) >=2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets < remainingTickets	
+		
+		if isValidName && isValidEmail && isValidTicketNumber {
 			remainingTickets = remainingTickets - userTickets
 			bookings = append(bookings, firstName + " " + lastName)
 			
@@ -52,7 +56,15 @@ func main() {
 				break
 			}	
 		} else {
-			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets.\n", remainingTickets, userTickets)
+			if !isValidName {
+				fmt.Println("First name or Last name you entered is too short")
+			}
+			if !isValidEmail{
+				fmt.Println("Email address doesn't contain @ sign")
+			}
+			if isValidTicketNumber {
+				fmt.Println("Number of Ticket you entered is invalid")	
+			}
 		}
 	}
 }
